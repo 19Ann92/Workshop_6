22f7b3bbadc58ae8dca1d284c0fd071d54ade943
Anna Eliseeva <Panterka6868@mail.ru>
2022-12-11 18:09:36 +0300

    add Task with an asterisk 2

---
+++ Task_with_an_asterisk_2/Program.cs
@@ -0,0 +1,41 @@
+﻿// Дополнительная задача 2 (задача со звёздочкой): Напишите программу, 
+// которая задаёт массив из n элементов, которые необходимо заполнить 
+// случайными значениями и определить существует ли пара соседних 
+// элементов с одинаковыми значениями, при наличии такого элемента 
+// заменить его на уникакальное значение.
+// [1,2,3,3] -> [1,2,3,4]
+
+Console.Clear();
+
+Console.Write("Введите размерность массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] FillingArray(int n)
+{
+    Random rnd = new Random();
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = rnd.Next(1, 10);
+    }
+    return arr;
+}
+
+int[] arr = FillingArray(num);
+Console.WriteLine($"[{String.Join(", ", arr)}]");
+
+int[] ReplaceArrayElement(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        if (array[i] == array[i + 1])
+        {
+            array[i + 1] = 0;
+        }
+    }
+    return array;
+}
+
+int[] modifiedArray = ReplaceArrayElement(arr);
+Console.WriteLine($"[{String.Join(", ", modifiedArray)}]");
+
